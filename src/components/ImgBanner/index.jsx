b8deb6cf78bda868b712e153b33512f7ca26846b
huzaifa311 <huzaifahabib311@gmail.com>
2024-05-24 04:08:51 +0500

    unblur

--- src/components/ImgBanner/index.jsx
+++ src/components/ImgBanner/index.jsx
@@ -7,7 +7,7 @@ const ImgBanner = ({ className, src }) => <>
     <section className={`w-[98.75vw] flex justify-center ${className}`} id='events'>
         <div className='w-[80vw] 2xl:w-[1500px]'>
             <h3 className='sm:text-[2.7rem] text-[2.4rem] text-[#C9C8CA] font-thin font-sans uppercase text-center sm:-mb-4 '>Get your Tickets Now to the Hottest</h3>
-            <h1 className='uppercase text-[#FFFFFF] sm:text-[5.5rem] text-[5.1rem] font-sans font-extrabold text-center leading-[90px]'>upcoming events</h1>
+            <h1 className='uppercase text-[#FFFFFF] sm:text-[5.5rem] text-[4rem] font-sans font-extrabold text-center leading-[90px]'>upcoming events</h1>
         </div>
     </section>
 </>
