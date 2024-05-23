f21b8bdaf51328eea5e235da9a3e4b471eb6ef2c
huzaifa311 <huzaifahabib311@gmail.com>
2024-05-23 23:12:11 +0500

    update

--- src/app/page.js
+++ src/app/page.js
@@ -6,29 +6,27 @@ import data from "../constant/data.json";
 import { SessionProvider } from "next-auth/react";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <Banner video={data.videos.video1} className={"mt-14"} />
-      <div className="mt-28 overflow-x-hidden">
-        <ProductCarousel className="" />
-        <ProductCarousel className="pt-20" />
+  return <>
+    <Header />
+    <Banner video={data.videos.video1} className={"mt-14"} />
+    <div className="mt-28 overflow-x-hidden">
+      <ProductCarousel className="" />
+      <ProductCarousel className="pt-20" />
 
-        {/* <Upcoming /> */}
-        <VideoCont />
-        <ProductCarousel className="pt-52" />
-        <Marquee />
-        <Banner video={data.videos.video2} className="mt-52" />
-        {/* <UpcomingEvent /> */}
-        <Highlights />
-        <Marquee />
-        <Banner video={data.videos.video3} className="mt-44" />
-        <ProductCarousel className="pt-52" />
-        <MarqueeImgs />
-        {/* <Testimonials /> */}
-        {/* <Agenda /> */}
-      </div>
-      <Footer />
-    </>
-  )
+      {/* <Upcoming /> */}
+      <VideoCont />
+      <ProductCarousel className="pt-52" />
+      <Marquee />
+      <Banner video={data.videos.video2} className="mt-52" />
+      {/* <UpcomingEvent /> */}
+      <Highlights />
+      <Marquee />
+      <Banner video={data.videos.video3} className="mt-44" />
+      <ProductCarousel className="pt-52" />
+      <MarqueeImgs />
+      {/* <Testimonials /> */}
+      {/* <Agenda /> */}
+    </div>
+    <Footer />
+  </>
 }
