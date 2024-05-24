691a274fc70a14899e1a3ec960e80beb6429a387
huzaifa311 <huzaifahabib311@gmail.com>
2024-05-25 04:05:53 +0500

    update

--- src/app/create-event/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import CreateEvent from '@/components/events/CreateEvent'
-const page = () => {
-  return (
-    <CreateEvent/>
-  )
-}
-
-export default page
--- src/redux/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import floorSlice from './reducers/floorSlice';
-
-export const store = configureStore({
-  reducer: {
-    floorData: floorSlice, //.reducer
-  },
-});
