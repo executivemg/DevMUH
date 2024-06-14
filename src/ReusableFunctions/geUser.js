16418318f23d346e200022262c726add93423da8
executivemg <167598354+executivemg@users.noreply.github.com>
2024-06-15 02:33:31 +0500

    Delete src directory

--- src/ReusableFunctions/geUser.js
+++
@@ -1,8 +0,0 @@
-const userInfo = () => {
-    if (typeof window !== "undefined") {
-        return JSON.parse(localStorage.getItem("user"));
-    }
-    return null;
-};
-
-export default userInfo;
--- src/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "https://workbytech.host/eventrush/api",
-});
-
-export default instance;
