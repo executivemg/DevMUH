67df8f12bf8770cda72102a656642241f76b64df
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-06-20 21:55:18 +0100

    setting up the api for the create event

--- src/app/event/components/seat_picker/index.jsx
+++ src/app/event/components/seat_picker/index.jsx
@@ -1,27 +1,59 @@
 import React, { useState } from 'react';
-import { Modal, SwipeableDrawer } from '@mui/material';
+import { Modal, SwipeableDrawer, Backdrop } from '@mui/material';
 
-const SeatModal = ({ seat }) => {
-  const [open, setOpen] = useState(false);
+const cl = console.log.bind(console);
 
+const SeatModal = ({ open, onClose, seat }) => {
   return (
     <Modal
       open={open}
-      onClose={handleClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
+      onClose={onClose}
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        sx: { backgroundColor: 'rgba(255,255,255,0.2)' }, 
+      }}
     >
-      <div>
-        <h2>/{seat.alias}</h2>
-        <div>
-          <p className='text-gray text-[0.875rem] '>
-            Name: <span className='font-[600] text-gray-light'>{seat.name}</span>
-          </p>
+      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-[25rem] bg-primary p-[1rem] md:p-[2rem] rounded-[8px] ">
+        <h2 className="font-[700] text-[2rem] text-white mx-auto w-max mb-[1rem] ">/{seat.alias}</h2>
+        <div className="flex flex-col gap-[1rem] w-full">
+          <div className='flex gap-[0.5rem] items-center '>
+            <p className='text-[11px] font-[600] uppercase text-base'>Name:</p> 
+            <span className='text-[14px] text-white/90 capitalize '>{seat.name}</span>
+          </div>
+          <div className='flex gap-[0.5rem] items-center '>
+            <p className='text-[11px] font-[600] uppercase text-base'>Price:</p> 
+            <span className='text-[14px] text-white/90 capitalize '>${seat.price}</span>
+          </div>
+          <div className='flex justify-evenly text-center gap-[1rem] w-full h-max'>
+            <div className='flex flex-col text-center items-center '>
+              <p className='text-[11px] font-[600] uppercase text-base'>People</p> 
+              <span className='text-[14px] text-white/90 capitalize '>{seat.people}</span>
+            </div>
+            <hr className='h-full w-[1px] bg-gray/20' />
+            <div className='flex flex-col text-center items-center '>
+              <p className='text-[11px] font-[600] uppercase text-base'>Serveware</p> 
+              <span className='text-[14px] text-white/90 capitalize '>{seat.serveware}</span>
+            </div>
+          </div>
+          <div className='flex flex-col gap-[0.5rem] items-center '>
+            <p className='text-[11px] font-[600] uppercase text-base'>Description:</p> 
+            <span className='text-[14px] text-white/90 capitalize '>{seat.desc}</span>
+          </div>
+        </div>
+
+        <div className='flex flex-col gap-[1rem] items-center w-full mt-[1rem] '>
+          <button
+            className='flex items-center justify-center text-center w-full max-w-[15rem] h-[2.875rem] text-[0.875rem] rounded-[32px] bg-base text-white hover:bg-base-hover active:bg-opacity-[70%] ease-200'
+          >Buy Ticket</button>
+          <button
+            onClick={onClose}
+            className='flex items-center justify-center text-center w-full max-w-[15rem] h-[2.875rem] text-[0.875rem] rounded-[32px] bg-gray/10 text-white hover:bg-gray/20 border-solid border-[1px] border-white/50 active:bg-opacity-[70%] ease-200'
+          >Cancel Purchase</button>
         </div>
       </div>
     </Modal>
-  )
-}
+  );
+};
 
 const drawerBleeding = 56;
 
@@ -33,39 +65,50 @@ const SeatPicker = ({ open, toggleDrawer, seats }) => {
   const handleOnClick = (seat) => {
     setModalData(seat);
     setOpenModal(true);
-  }
+    cl('new seat:', modalData);
+    cl('seat:', seat);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+    setModalData(null);
+  };
 
   return (
-    <SwipeableDrawer
-      anchor="bottom"
-      open={open}
-      onClose={toggleDrawer(false)}
-      onOpen={toggleDrawer(true)}
-      swipeAreaWidth={drawerBleeding}
-      disableSwipeToOpen={false}
-      ModalProps={{
-        keepMounted: true,
-      }}
-    >
-      {openModal && <SeatModal seat={modalData} />}
-      <div className='h-[80vh] md:h-[60vh] overflow-x-auto w-full bg-primary shadow-[0_0_2px_4px_rgba(150,150,255,0.2)] '>
-        <div className='flex flex-col gap-[2rem] p-[2rem] mx-auto max-w-[60rem] '>
-          <h2 className='text-base uppercase font-[600] '>List of available seats</h2>
-          <div className='grid grid-cols-[repeat(auto-fill,minmax(3rem,1fr))] gap-[0.875rem] md:gap-[1rem] '>
-            {seats.map((seat, index) => (
-              <button
-                key={index}
-                onClick={()=>handleOnClick(seat)}
-                className='group flex items-center justify-center text-center h-[3rem] w-[3rem] rounded-[4px] bg-gray/10 hover:bg-gray/20 ease-250 active:bg-white/50 active:text-base hover:scale-[1.01] border-solid border-white/10 border-0 hover:border-[1px] '
-              >
-                <p className='font-[600] text-gray-light text-[0.875rem] group-hover:text-base ease-250 '>{seat.alias}</p>
-              </button>
-            ))}
+    <div>
+      <SwipeableDrawer
+        anchor="bottom"
+        open={open}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+        swipeAreaWidth={drawerBleeding}
+        disableSwipeToOpen={false}
+        ModalProps={{
+          keepMounted: true,
+        }}
+      >
+        <div className='h-[80vh] md:h-[60vh] overflow-x-auto w-full bg-primary shadow-[0_0_2px_4px_rgba(150,150,255,0.2)]'>
+          <div className='flex flex-col gap-[2rem] p-[2rem] mx-auto max-w-[60rem]'>
+            <h2 className='text-base uppercase font-[600]'>List of available seats</h2>
+            <div className='grid grid-cols-[repeat(auto-fill,minmax(3rem,1fr))] gap-[0.875rem] md:gap-[1rem]'>
+              {seats.map((seat, index) => (
+                <button
+                  key={index}
+                  onClick={() => handleOnClick(seat)}
+                  className='group flex items-center justify-center text-center h-[3rem] w-[3rem] rounded-[4px] bg-gray/10 hover:bg-gray/20 ease-250 active:bg-white/50 active:text-base hover:scale-[1.01] border-solid border-white/10 border-0 hover:border-[1px]'
+                >
+                  <p className='font-[600] text-gray-light text-[0.875rem] group-hover:text-base ease-250'>{seat.alias}</p>
+                </button>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    </SwipeableDrawer>
+      </SwipeableDrawer>
+      {modalData && (
+        <SeatModal open={openModal} onClose={handleCloseModal} seat={modalData} />
+      )}
+    </div>
   );
-}
+};
 
 export default SeatPicker;
--- src/app/event/page.jsx
+++ src/app/event/page.jsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import seat from './floorplan';
 import SeatPicker from './components/seat_picker';
+import { RiZoomInLine, RiZoomOutLine } from "react-icons/ri";
 
 const EventPage = () => {
   const [eventData, setEventData] = React.useState({
@@ -34,6 +35,9 @@ const EventPage = () => {
     setOpenSeats(newOpen);
   };
 
+  // ::::::::::::::::::::::::::: IMAGE STATE
+  const [openImage, setOpenImage] = useState(false);
+
   return (
     <div className='w-full min-h-screen text-white py-[4rem] font-poppins '>
       
@@ -44,99 +48,110 @@ const EventPage = () => {
         seats={eventData.floorplanLayout} 
       />
 
-      <div className='relative flex max-md:flex-col gap-y-[3rem] max-w-[75rem] mx-auto p-[1rem] md:p-[2rem] '>
+      <div className='relative flex max-lg:flex-col gap-y-[3rem] max-w-[75rem] mx-auto p-[2rem] md:max-lg:px-[4rem]'>
         {/* :::::::::::::::::::::::: IMAGE INFO */}
-        <div className='md:sticky top-[2rem] left-0 flex flex-col w-full md:w-[50%] md:h-[40rem] rounded-[16px] overflow-hidden shadow-[0_0_2px_4px_rgba(150,150,255,0.2)] '>
+        <div className='lg:sticky top-[2rem] left-0 flex flex-col w-full lg:w-[50%] lg:h-[40rem] rounded-[16px] overflow-hidden shadow-[0_0_4px_2px_rgba(150,150,255,0.2)] '>
           <img
             src={eventData?.image || '/assets/images/mode_0_event_plan.jpg'}
             alt='Event'
             className='relative z-1 w-full h-full object-contain '
           />
 
-          <div className='relative blur-[5px] w-full h-0 '>
+          <div className='relative w-full h-0 '>
             <img
               src={eventData?.image || '/assets/images/mode_0_event_plan.jpg'}
               alt='Event'
               className='absolute z-[-1] bottom-0 left-0 blur-[5px] w-full h-full object-cover '
             />
+
+            <button
+              onClick={()=>setOpenImage(!openImage)}
+              className='absolute z-[100] bottom-[1rem] right-[1rem] flex items-center justify-center w-[3rem] h-[3rem] rounded-[8px] bg-white text-primary hover:bg-base hover:text-white ease-250 border-solid border-primary border-[1px] '
+            >
+              {openImage? 
+              <RiZoomOutLine className='text-[1.5rem]' /> : 
+              <RiZoomInLine className='text-[1.5rem]' />}
+            </button>
           </div>
         </div>
 
         {/* :::::::::::::::::::::::: TEXT INFORMATION */}
-        <div className='flex flex-col gap-[1rem] w-full md:max-w-[35rem] md:px-[4rem] '>
-          {/* :::::::::::::::: EVENT TITLE */}
-          <h2 className='text-white font-[700] text-[2.5rem] '>{eventData?.eventName}</h2>
-          
-          {/* :::::::::::::::: EVENT PRICE */}
-          <div className='flex items-center gap-[1rem] '>
-            <p className='text-[1rem] font-[700] border-solid border-base border-[2px] rounded-[4px] text-base px-[0.5rem] py-[0.125rem] hover:scale-[1.03] hover:shadow-[0_0_5px_2px_rgba(255,255,255,0.2)] ease-250 cursor-pointer '>${eventData?.ticketPrice}</p>
-            <p className='text-gray text-[1.05rem] font-[700] '>{Number(eventData?.ticketQuantity) - Number(eventData?.ticketSold)} seats available </p>
-          </div>
+        <div className='flex justify-center w-full lg:w-[50%] lg:px-[2rem] '>
+          <div className='flex flex-col gap-[1rem] w-full lg:max-w-[30rem] '>
+            {/* :::::::::::::::: EVENT TITLE */}
+            <h2 className='text-white font-[700] text-[2.5rem] md:text-[3rem] '>{eventData?.eventName}</h2>
+            
+            {/* :::::::::::::::: EVENT PRICE */}
+            <div className='flex items-center gap-[1rem] '>
+              <p className='text-[1rem] font-[700] border-solid border-base border-[2px] rounded-[4px] text-base px-[0.5rem] py-[0.125rem] hover:scale-[1.03] hover:shadow-[0_0_5px_2px_rgba(255,255,255,0.2)] ease-250 cursor-pointer '>${eventData?.ticketPrice}</p>
+              <p className='text-gray text-[1.05rem] font-[700] '>{Number(eventData?.ticketQuantity) - Number(eventData?.ticketSold)} seats available </p>
+            </div>
 
-          {/* :::::::::::::::: EVENT DESCRIPTION */}
-          <p className='text-[1rem] text-gray font-[400] '>{eventData?.description}</p>
-          <a 
-            href={`${window.location.origin}/event/${eventData?.domain}`}
-            className='underline text-base font-[400] text-[1rem] '
-          
-          >{window.location.origin}/event/{eventData?.domain}</a>
+            {/* :::::::::::::::: EVENT DESCRIPTION */}
+            <p className='text-[1rem] text-gray font-[400] '>{eventData?.description}</p>
+            <a 
+              href={`${window.location.origin}/event/${eventData?.domain}`}
+              className='underline text-base font-[400] text-[1rem] '
+            
+            >{window.location.origin}/event/{eventData?.domain}</a>
 
-          {/* :::::::::::::::: EVENT OWNER INFO */}
-          <div className='flex flex-col gap-[0.25rem]'>
-            <div className='flex gap-[1rem] items-center'>
-              <img
-                src={eventData?.owner_image || '/assets/images/mode_0_event_plan.jpg'}
-                alt=''
-                className='w-[3rem] h-[3rem] min-w-[3rem] object-cover rounded-[50rem] '
-              />
+            {/* :::::::::::::::: EVENT OWNER INFO */}
+            <div className='flex flex-col gap-[0.25rem]'>
+              <div className='flex gap-[1rem] items-center'>
+                <img
+                  src={eventData?.owner_image || '/assets/images/mode_0_event_plan.jpg'}
+                  alt=''
+                  className='w-[3rem] h-[3rem] min-w-[3rem] object-cover rounded-[50rem] '
+                />
+                <div className='flex flex-col '>
+                  <p className='text-gray text-[0.875rem] md:text-[1rem]'>Owner (Host)</p>
+                  <p className='text-gray-light text-[0.875rem] md:text-[1rem]'>{eventData?.name || 'Luthor'}</p>
+                </div>
+              </div>
+              <p className='text-gray text-[0.875rem] md:text-[1rem]'>Contact: <span className='text-gray-light'>{eventData?.contactPhone}</span></p>
+              <p className='text-gray text-[0.875rem] md:text-[1rem]'>Email: <span className='text-gray-light'> {eventData?.contactEmail}</span></p>
+            </div>
+
+            {/* :::::::::::::::::: TIME & LOCATION */}
+            <div className='flex flex-col '>
+              <p className='text-gray-light text-[0.875rem] md:text-[1rem] uppercase font-[600] mt-[1rem] '>Location</p>
+
               <div className='flex flex-col '>
-                <p className='text-gray text-[0.875rem]'>Owner (Host)</p>
-                <p className='text-gray-light text-[0.875rem]'>{eventData?.name || 'Luthor'}</p>
+                {/* :::::::::::::::::::::: LOCATION */}
+                <p className='text-gray text-[0.875rem] md:text-[1rem]'>Address: <span className='text-gray-light'>{eventData?.address}</span></p>
+                <p className='text-gray text-[0.875rem] md:text-[1rem]'>Zip code: {eventData?.zipCode}</p>
+                <p className='text-gray text-[0.875rem] md:text-[1rem]'>City: <span className='text-gray-light'>{eventData?.state}</span></p>
+                <p className='text-gray text-[0.875rem] md:text-[1rem]'>State: <span className='text-gray-light'>{eventData?.city}</span></p>
+              </div>
+
+              <p className='text-gray-light text-[0.875rem] md:text-[1rem] uppercase font-[600] mt-[1rem] '>Time</p>       
+
+              <div className='flex flex-col '>
+                {/* :::::::::::::::::::::: TIME  */}
+                <p className='text-gray text-[0.875rem] md:text-[1rem]'>From: <span className='text-gray-light'>{eventData?.startTime}</span></p>
+                <p className='text-gray text-[0.875rem] md:text-[1rem]'>To: <span className='text-gray-light'> {eventData?.endTime}</span></p>
+              </div>
+              
+              <p className='text-gray-light text-[0.875rem] md:text-[1rem] uppercase font-[600] mt-[1rem] '>Date</p>       
+
+              <div className='flex flex-col '>
+                {/* :::::::::::::::::::::: DATE  */}
+                <p className='text-gray text-[0.875rem] md:text-[1rem]'>From: <span className='text-gray-light'>{eventData?.startDate}</span></p>
+                <p className='text-gray text-[0.875rem] md:text-[1rem]'>To: <span className='text-gray-light'>{eventData?.endDate}</span></p>
               </div>
             </div>
-            <p className='text-gray text-[0.875rem]'>Contact: <span className='text-gray-light'>{eventData?.contactPhone}</span></p>
-            <p className='text-gray text-[0.875rem]'>Email: <span className='text-gray-light'> {eventData?.contactEmail}</span></p>
-          </div>
 
-          {/* :::::::::::::::::: TIME & LOCATION */}
-          <div className='flex flex-col '>
-            <p className='text-gray-light text-[0.875rem] uppercase font-[600] mt-[1rem] '>Location</p>
-
-            <div className='flex flex-col '>
-              {/* :::::::::::::::::::::: LOCATION */}
-              <p className='text-gray text-[0.875rem]'>Address: <span className='text-gray-light'>{eventData?.address}</span></p>
-              <p className='text-gray text-[0.875rem]'>Zip code: {eventData?.zipCode}</p>
-              <p className='text-gray text-[0.875rem]'>City: <span className='text-gray-light'>{eventData?.state}</span></p>
-              <p className='text-gray text-[0.875rem]'>State: <span className='text-gray-light'>{eventData?.city}</span></p>
+            {/* :::::::::::::::::::::::::: CTA */}
+            <div className='flex flex-col gap-[1rem] items-center w-full p-[2rem] rounded-[8px] border-solid border-gray/20 border-[1px] shadow-[0_0_2px_4px_rgba(150,150,255,0.1)] '>
+              <p className='text-gray text-[0.875rem] md:text-[1rem] font-[600] '>Don&apos;t have a ticket? Get your tickets with ease now.</p>
+              <button
+                onClick={()=>setOpenSeats(!openSeats)}
+                className='flex items-center justify-center text-center w-full max-w-[22rem] h-[3rem] text-[0.875rem] md:text-[1rem] font-[600] rounded-[32px] bg-primary text-white hover:text-black hover:bg-gray-light border-solid border-[1px] border-white/20 active:bg-opacity-[70%] ease-200'
+              >Buy Seat Ticket</button>
+              <button
+                className='flex items-center justify-center text-center w-full max-w-[22rem] h-[3rem] text-[0.875rem] md:text-[1rem] font-[600] rounded-[32px] bg-base text-white hover:bg-base-hover border-solid border-[1px] border-white/20 active:bg-opacity-[70%] ease-200'
+              >Quick Buy</button>
             </div>
-
-            <p className='text-gray-light text-[0.875rem] uppercase font-[600] mt-[1rem] '>Time</p>       
-
-            <div className='flex flex-col '>
-              {/* :::::::::::::::::::::: TIME  */}
-              <p className='text-gray text-[0.875rem]'>From: <span className='text-gray-light'>{eventData?.startTime}</span></p>
-              <p className='text-gray text-[0.875rem]'>To: <span className='text-gray-light'> {eventData?.endTime}</span></p>
-            </div>
-            
-            <p className='text-gray-light text-[0.875rem] uppercase font-[600] mt-[1rem] '>Date</p>       
-
-            <div className='flex flex-col '>
-              {/* :::::::::::::::::::::: DATE  */}
-              <p className='text-gray text-[0.875rem]'>From: <span className='text-gray-light'>{eventData?.startDate}</span></p>
-              <p className='text-gray text-[0.875rem]'>To: <span className='text-gray-light'>{eventData?.endDate}</span></p>
-            </div>
-          </div>
-
-          {/* :::::::::::::::::::::::::: CTA */}
-          <div className='flex flex-col gap-[1rem] items-center w-full p-[2rem] rounded-[8px] border-solid border-gray/50 border-[1px] shadow-[0_0_2px_4px_rgba(150,150,255,0.2)] '>
-            <p className='text-gray text-[0.875rem] font-[600] '>Don&apos;t have a ticket? Get your tickets with ease now.</p>
-            <button
-              onClick={()=>setOpenSeats(!openSeats)}
-              className='flex items-center justify-center text-center w-full max-w-[22rem] h-[3rem] text-[0.875rem] font-[600] rounded-[32px] bg-primary text-white hover:text-black hover:bg-gray-light border-solid border-[1px] border-white/20 active:bg-opacity-[70%] ease-200'
-            >Buy Seat Ticket</button>
-            <button
-              className='flex items-center justify-center text-center w-full max-w-[22rem] h-[3rem] text-[0.875rem] font-[600] rounded-[32px] bg-base text-white hover:bg-base-hover border-solid border-[1px] border-white/20 active:bg-opacity-[70%] ease-200'
-            >Quick Buy</button>
           </div>
         </div>
       </div>
